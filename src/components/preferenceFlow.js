5555a7cc70f176fe9caf2210091e6b7db79b6d0b
Miles Sorce <m.sorce@gmail.com>
2017-12-09 18:12:18 -0800

    Add preference flow & menu components

---
+++ src/components/preferenceFlow.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import Columns from "react-columns";
+import { Button, Icon } from 'semantic-ui-react';
+
+const genres = ['Action', 'Adventure', 'Animation', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'History', 'Horror', 'Music', 'Mystery', 'Romance', 'Science Fiction', 'Thriller', 'War', 'Western'];
+const queries = [
+  {
+    columns: 2,
+    query: "min-width: 440px",
+    key: 2
+  },
+  {
+    columns: 3,
+    query: "min-width: 680px",
+    key: 3
+  },
+  {
+    columns: 4,
+    query: "min-width: 920px",
+    key: 4
+  },
+  {
+    columns: 5,
+    query: "min-width: 1160px",
+    key: 5
+  }
+];
+
+class PreferenceFlow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clickedGenres: {},
+      mode: 'favorite'
+    }
+    this.handleFavoriteClick = this.handleFavoriteClick.bind(this);
+    this.handleDislikeClick = this.handleDislikeClick.bind(this);
+    this.handleContinueClick = this.handleContinueClick.bind(this);
+  }
+
+  handleFavoriteClick(genre, e) {
+    let genres = this.state.clickedGenres;
+    if (genres[genre] && genres[genre] === 'favorite') {
+      delete genres[genre];
+    } else if (!genres[genre]) {
+      genres[genre] = 'favorite';
+    }
+    this.setState({
+      clickedGenres: genres
+    });
+  }
+
+  handleDislikeClick(genre, e) {
+    let genres = this.state.clickedGenres;
+    if (genres[genre] && genres[genre] === 'dislike') {
+      delete genres[genre];
+    } else if (!genres[genre]) {
+      genres[genre] = 'dislike';
+    }    
+    this.setState({
+      clickedGenres: genres
+    });
+  }
+
+  handleContinueClick(e) {    
+    this.setState({
+      mode: 'dislike'
+    });
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    return (
+      <div style={{textAlign: 'center', paddingBottom: '20px'}}>
+        {
+          this.state.mode === 'favorite' ? 
+          <h1>Click your favorite genres</h1> :
+          <div>
+            <h1>Do you dislike any genres?</h1>
+            <h3>These genres will be excluded from your recommendations</h3>
+          </div>
+        }
+        <div className='genres' style={{paddingBottom: '15px'}}>
+          <Columns queries={queries}>
+            {genres.map(genre => (
+              <div 
+                className='genre-box'
+                value={genre}
+                onClick={this.state.mode === 'favorite' ? (e)=>this.handleFavoriteClick(genre, e) : (e)=>this.handleDislikeClick(genre, e)}
+              >
+                <h3>{genre}</h3>
+                {this.state.clickedGenres[genre] ? 
+                  (this.state.clickedGenres[genre] === 'favorite' ? 
+                    <Icon name='checkmark' size='big' color='green' style={{position: 'absolute', paddingTop: '10rem', paddingLeft: '9.5rem'}}/> 
+                    : <Icon name='ban' size='big' color='red' style={{position: 'absolute', paddingTop: '10rem', paddingLeft: '9.5rem'}}/>) : null
+                }
+              </div>
+            ))}
+          </Columns>
+        </div>
+        <Button 
+          size='massive'
+          color='google plus'
+          onClick={this.state.mode === 'favorite' ? this.handleContinueClick : null}
+        >
+        {this.state.mode === 'favorite' ? 'Continue' : 'Finish'}
+        </Button>
+      </div>
+    )
+  }
+};
+
+export default PreferenceFlow;
