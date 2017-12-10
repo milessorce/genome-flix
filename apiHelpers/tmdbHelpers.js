7a86f82fc1dd2572fb47421c6b56b95af71c5c0f
Victor Ngo <psychosete@gmail.com>
2017-12-10 14:08:20 -0800

    (feat) Update genreIds to be saved onto db and on tmdb helpers

--- apiHelpers/tmdbHelpers.js
+++ apiHelpers/tmdbHelpers.js
@@ -1,27 +1,46 @@
 const tmdb = require('./tmdb.js');
 const request = require('request');
 const Promise = require('bluebird');
+const dbHelpers = require('../database-mysql/helpers.js');
+
 
 var genreIds = {
   "Action": 28,
-  "Adventure": 12,
   "Animation": 16,
   "Comedy": 35,
-  "Crime": 80,
   "Documentary": 99,
   "Drama": 18,
   "Family": 10751,
-  "Fantasy": 14,
-  "History": 36,
   "Horror": 27,
   "Music": 10402,
-  "Mystery": 9648,
   "Romance": 10749,
   "Science Fiction": 878,
   "Thriller": 53,
   "War": 10752,
   "Western": 37
-}
+};
+
+// OLD GENREIDs
+// var genreIds = {
+//   "Action": 28,
+//   "Adventure": 12,
+//   "Animation": 16,
+//   "Comedy": 35,
+//   "Crime": 80,
+//   "Documentary": 99,
+//   "Drama": 18,
+//   "Family": 10751,
+//   "Fantasy": 14,
+//   "History": 36,
+//   "Horror": 27,
+//   "Music": 10402,
+//   "Mystery": 9648,
+//   "Romance": 10749,
+//   "Science Fiction": 878,
+//   "Thriller": 53,
+//   "War": 10752,
+//   "Western": 37
+// };
 
 var MovieList = {
   getMoviesByGenre: (cb) => {
--- database-mysql/helpers.js
+++ database-mysql/helpers.js
@@ -26,18 +26,13 @@ var clearTables = () => {
 
       var genreIds = {
         "Action": 28,
-        "Adventure": 12,
         "Animation": 16,
         "Comedy": 35,
-        "Crime": 80,
         "Documentary": 99,
         "Drama": 18,
         "Family": 10751,
-        "Fantasy": 14,
-        "History": 36,
         "Horror": 27,
         "Music": 10402,
-        "Mystery": 9648,
         "Romance": 10749,
         "Science Fiction": 878,
         "Thriller": 53,
